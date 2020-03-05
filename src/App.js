2a3a0d2c74311a0123b28c1d73a2dcde00736899
Gia Edgington <gia.edgington@gmail.com>
2020-03-04 19:36:59 -0500

    Import Client, Provider, Cache / Create client

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache({
+
+  })
+})
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ApolloProvider client={client}>
+      <div></div>
+    </ApolloProvider>
   );
 }
 
